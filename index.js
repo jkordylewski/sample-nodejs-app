d94408154ebb257fe44113b154eba478ea991d93
Jan Kordylewski <jan.kordylewski@domandtom.com>
2018-08-27 11:08:35 -0500

    one more update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hey, I\'m a Node.js app! -jkordy')
+    res.send('Hey, I\'m a Node.js app!')
 })
 
 app.get('/hello', (req, res) => {
